8e84189fe01bc6d4101cbf4cdda96f72612f06ad
chainal <chainal777@gmail.com>
2020-09-19 22:05:38 +0800

    后端简单完成

--- src/main/java/com/longsalix/web/controller/CommonController.java
+++ src/main/java/com/longsalix/web/controller/CommonController.java
@@ -1,16 +1,18 @@
 package com.longsalix.web.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.longsalix.web.dao.UserRepository;
 import com.longsalix.web.domain.User;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class CommonController {
@@ -35,9 +37,41 @@ public class CommonController {
         logger.info(body);
         User user = JSON.parseObject(body, User.class);
         logger.info("{}", user);
-        DateTime now = new DateTime();
-        user.setCreate_time(now.toString("yyyyMMdd HH:mm:ss"));
-        user = userRepository.save(user);
-        return JSON.toJSONString(user);
+        List<User> existUser = userRepository.findUserByName(user.getName());
+        if (existUser.isEmpty()) {
+            DateTime now = new DateTime();
+            user.setId(null);
+            user.setCreate_time(now.toString("yyyy/MM/dd HH:mm:ss"));
+            userRepository.save(user);
+            return "SUCCESS";
+        } else {
+            return "EXISTED";
+        }
+    }
+
+    @GetMapping("/user/list")
+    @ResponseBody
+    public String listUser(@RequestParam(value = "p", required = false, defaultValue = "1") int pageIndex) {
+        int pageSize = 10;
+        pageIndex = pageIndex - 1;
+        Page<User> users = userRepository.findAll(PageRequest.of(pageIndex, pageSize));
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("total", "" + users.getNumberOfElements() + "/" + users.getTotalElements());
+        jsonObject.put("page", "" + (pageIndex + 1) + "/" + users.getTotalPages());
+        jsonObject.put("data", users.getContent());
+        return jsonObject.toJSONString();
+    }
+
+    @PostMapping("/user/delete")
+    @ResponseBody
+    public String deleteUser(@RequestBody String body) {
+        try {
+            User user = JSON.parseObject(body, User.class);
+            userRepository.deleteById(user.getId());
+            return "SUCCESS";
+        } catch (Exception e) {
+            logger.error("", e);
+            return "FAILED";
+        }
     }
 }
--- src/main/java/com/longsalix/web/dao/UserRepository.java
+++ src/main/java/com/longsalix/web/dao/UserRepository.java
@@ -1,7 +1,13 @@
 package com.longsalix.web.dao;
 
 import com.longsalix.web.domain.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.List;
+
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+    @Query(value = "select t from User t where t.name=?1 ")
+    List<User> findUserByName(String username);
 }
